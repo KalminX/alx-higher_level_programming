894b3a5c2d1049eeea5f3d4cf2257c3c7908fefe
KalminX <noriabure@gmail.com>
2023-10-31 13:45:27 -0700

    introduced the format

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if i != 99:
-        print(f"{i:0>2}", end=', ')
+        print("{:0>2}".format(i), end=', ')
     else:
-        print(i)
+        print("{}".format(i))
