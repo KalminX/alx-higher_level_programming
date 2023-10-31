b9c8a5fa5619144ae13ee9b604736d7fad2dc6c6
KalminX <noriabure@gmail.com>
2023-10-31 12:19:59 -0700

    Fixed the negative remainder

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,12 +8,12 @@ if number >= 0:
     elif last_digit == 0:
         print(f"Last digit of {number} is 0 and is 0")
     elif last_digit < 6 and last_digit != 0:
-        print(f"Last digit of {number} is {10 - last_digit} and is less than 6 and not 0")
+        print(f"Last digit of {number} is
+                {10 - last_digit} and is less than 6 and not 0")
 elif number < 0:
-    last_digit = 10 - (number % 10)
-    if last_digit > 5:
-        print(f"Last digit of {number} is -{last_digit} and is greater than 5")
-    elif last_digit == 0:
+    last_digit = (number % 10) - 10
+    if last_digit == 0:
         print(f"Last digit of {number} is 0 and is 0")
     elif last_digit < 6 and last_digit != 0:
-        print(f"Last digit of {number} is -{last_digit} and is less than 6 and not 0")
+        print(f"Last digit of {number} is 
+        -{last_digit} and is less than 6 and not 0")
