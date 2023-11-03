c203ac648dc872740aa8b800908421c21f794d62
KalminX <noriabure@gmail.com>
2023-11-02 17:28:48 -0700

    Used the dir function

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[0] == '_':
+            continue
+        print(name)
