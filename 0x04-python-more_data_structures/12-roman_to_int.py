ef93ea6761b96257326e9df7b9215223b377f23e
KalminX <noriabure@gmail.com>
2023-11-13 22:36:45 -0800

    Created a function that converts roman to integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string.isalpha():
+    if isinstance(roman_string, str):
         roman_int_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         roman_reversed = roman_string[::-1]
         number = 0
