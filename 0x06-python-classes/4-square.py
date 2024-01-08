5a869569734d2c0e1f1e441d9433fd0354ad16ac
Kalmin <noriabure@gmail.com>
2024-01-08 14:19:35 -0800

    Worked on the fifth tasks

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""4-square.py"""
+
+
+class Square:
+    """A square class"""
+    def __init__(self, size=0):
+        """The constructor method"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Calculates the area of the square"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Gets the size of the Square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of the square"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
