9c095ea3d269b36aad59737bbd4d39efb42c176c
Kalmin <noriabure@gmail.com>
2024-01-09 03:20:02 -0800

    Worked on my first task in test driven development

---
+++ 0x07-python-test_driven_developmen/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""0-add_integer.py"""
+
+
+def add_integer(a, b=98):
+    """Adds two numbers"""
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    elif type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
---
+++ 0x07-python-test_driven_developmen/README.md
@@ -0,0 +1,1 @@
+Learning test driven development
