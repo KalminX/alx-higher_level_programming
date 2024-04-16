2a13f7d30cb28e596cf00460a09a047302075f45
KalminX <noriabure@gmail.com>
2024-04-16 03:38:12 +0100

    solved the third task

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""My lookup module"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Returns True if the object is an instance of, or if the object is an instance of a class that inherited from, the specified class"""
+    return isinstance(obj, a_class)
