e29b30a46fc5f7ea786f7180b6d320d146e254b3
KalminX <noriabure@gmail.com>
2024-04-16 02:55:12 +0100

    added the super.__init__

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,11 +4,11 @@
 
 class MyList(list):
     """A class that inherits from list"""
+
     def __init__(self):
         """Initializes the object"""
-        pass
+        super.__init__()
 
     def print_sorted(self):
         """Prints the list, but sorted"""
         print(sorted(self))
-
