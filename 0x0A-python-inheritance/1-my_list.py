854d32712a63bf761d8ae3f5dad9799cf4f11341
KalminX <noriabure@gmail.com>
2024-04-16 02:36:47 +0100

    added the __init__ method

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,6 +4,10 @@
 
 class MyList(list):
     """A class that inherits from list"""
+    def __init__(self):
+        """Initializes the object"""
+        pass
+
     def print_sorted(self):
         """Prints the list, but sorted"""
         print(sorted(self))
