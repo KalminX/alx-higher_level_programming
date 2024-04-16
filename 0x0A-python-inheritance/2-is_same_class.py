21848f29985735d8998ca4f55dc900a19986556f
KalminX <noriabure@gmail.com>
2024-04-16 03:29:11 +0100

    switched isinstance with type

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""My module that checks the class"""
+
+
+def is_same_class(obj, a_class):
+    """Returns True if the object is exactly an instance of the specified class"""
+    return type(a) == a_class
