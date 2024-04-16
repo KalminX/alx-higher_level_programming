e4c9be17d63d7086e805b980a1903b4a38e36b64
KalminX <noriabure@gmail.com>
2024-04-16 02:35:17 +0100

    created a list class

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""My list module"""
+
+
+class MyList(list):
+    """A class that inherits from list"""
+    def print_sorted(self):
+        """Prints the list, but sorted"""
+        print(sorted(self))
