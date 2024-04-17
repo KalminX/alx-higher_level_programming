3a314ba65238f8bcce1d6df75d9f056cbcff20a4
KalminX <noriabure@gmail.com>
2024-04-17 17:25:10 +0100

    solved the eighth task

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -19,7 +19,7 @@ def stats():
             if status_code in status_counts:
                 status_counts[status_code] += 1
 
-    return print_statistics(total_file_size, status_counts)
+    return total_file_size, status_counts
         
 
 def print_statistics(total_file_size, status_counts):
@@ -27,3 +27,35 @@ def print_statistics(total_file_size, status_counts):
     for status_code, count in sorted(status_counts.items()):
         if count > 0:
             print(f"{status_code}: {count}")
+
+
+def main():
+    total_file_size = 0
+    status_counts = {200: 0, 301: 0, 400: 0,
+                     401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    lines_read = 0
+
+    try:
+        for line in sys.stdin:
+            lines_read += 1
+            parts = line.strip().split()
+            if len(parts) < 10:
+                continue
+            file_size = int(parts[-1])
+            total_file_size += file_size
+            status_code = int(parts[-2])
+            if status_code in status_counts:
+                status_counts[status_code] += 1
+
+            if lines_read % 10 == 0:
+                print_statistics(total_file_size, status_counts)
+                total_file_size = 0
+                status_counts = {200: 0, 301: 0, 400: 0,
+                                 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+
+    except KeyboardInterrupt:
+        print_statistics(total_file_size, status_counts)
+
+
+if __name__ == "__main__":
+    main()
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python3
 """My file module"""
+import sys
 
+
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+try:
+    data = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    data = []
+
+for index, arg in enumerate(sys.argv):
+    if index == 0:
+        continue
+    data.append(sys.argv[index])
+save_to_json_file(data, "add_item.json")
