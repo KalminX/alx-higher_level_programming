f76e335fca65f06d18d301d0ade33d4852291f37
KalminX <noriabure@gmail.com>
2024-04-17 18:22:11 +0100

    solved the eleventh task

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""My class module"""
+
+
+class Student:
+    """My class module"""
+
+    def __init__(self, first_name, last_name, age):
+        """Method to initialize the student object"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Method to append to a file"""
+        if attrs is not None:
+            return {key: value for key, value in self.__dict__.items()
+                    if key in attrs}
