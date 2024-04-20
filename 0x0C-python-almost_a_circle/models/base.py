3064fac40eef652406d53f6f18d9a4604a6d50b9
KalminX <noriabure@gmail.com>
2024-04-20 18:12:28 +0100

    Created a base class to manage all other classes

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""My base package"""
+
+
+class Base:
+    """Base class"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Init method"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
