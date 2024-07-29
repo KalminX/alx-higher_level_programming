ab9f9a7da0195d5876e2f7257260ce32ef640b98
KalminX <noriabure@gmail.com>
2024-07-29 21:00:36 +0100

    solved the tenth task

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+-- Creates a table called second_table in a database
+CREATE TABLE second_table(
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+INSERT INTO second_table VALUES
+    (1, "John", 10),
+    (1, "Alex", 3),
+    (1, "Bob", 14),
+    (1, "George", 8);
