cf3ea1cc8bc2682c5f4fdfdeac9388d5dbca0a97
KalminX <noriabure@gmail.com>
2024-07-30 18:37:32 +0100

    solved the second task

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- A script that creates a user
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
