b6b01ea6f66018b552fb464afb3bc1638ce45140
KalminX <noriabure@gmail.com>
2024-07-30 20:49:08 +0100

    solved the eighth task

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- A script that creates a database hbtn_0dd_usa and table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    FOREIGN KEY(state_id) REFERENCES states(id),
+    name VARCHAR(256) NOT NULL
+);
