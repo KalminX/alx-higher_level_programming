66b0ec1c8a67e91876bdb5522a3f07e0622a7ff4
KalminX <noriabure@gmail.com>
2024-07-30 20:53:13 +0100

    made foreign key not null

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,6 +3,6 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    FOREIGN KEY(state_id) REFERENCES states(id),
+    FOREIGN KEY(state_id) REFERENCES states(id) NOT NULL,
     name VARCHAR(256) NOT NULL
 );
