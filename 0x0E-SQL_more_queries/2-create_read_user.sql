111e9dc2e5c7ecaadf04a39eaf4dff1b9d9fcfe1
KalminX <noriabure@gmail.com>
2024-07-30 19:13:14 +0100

    solved task 3

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- A script that creates a database hbtn_0d2 and the user user_0d
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
