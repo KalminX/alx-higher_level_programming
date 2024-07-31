15548d55a04e6bd946e0c70ebd3ac13abe32b3b3
KalminX <noriabure@gmail.com>
2024-07-31 06:51:22 +0100

    solved the ninth task

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,13 @@
+-- A script that lists all cities contained in the database hbtn_0d_usa
+SELECT
+    cities.id AS id,
+    cities.name AS name,
+    states.name AS name
+FROM
+    cities
+INNER JOIN
+    states
+ON
+    cities.state_id = states.id
+ORDER BY
+    cities.id ASC;
