50d899ea856a0d152237b6d748d5f7d3a431b7c4
KalminX <noriabure@gmail.com>
2024-07-31 09:20:24 +0100

    made adjustments to task 10 and 11

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,10 +1,10 @@
--- A script that lists all shows contained in hbtn_0d_tvshows
+-- A script that lists all shows contained in the database hbtn_0d_tvshows
 USE hbtn_0d_tvshows;
 SELECT
     tv_shows.title AS title,
     tv_show_genres.genre_id AS genre_id
 FROM
-    tv_shows INNER JOIN tv_show_genres
+    tv_shows LEFT JOIN tv_show_genres
 ON
     tv_shows.id = tv_show_genres.show_id
 ORDER BY
