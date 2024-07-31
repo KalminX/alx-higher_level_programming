778d6370153508d9f3f43980f8b8fc57761bf3a7
KalminX <noriabure@gmail.com>
2024-07-31 04:32:21 +0100

    update: created a new field

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,6 +3,7 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    FOREIGN KEY(state_id) REFERENCES states(id) NOT NULL,
-    name VARCHAR(256) NOT NULL
+    state_id INT,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY(state_id) REFERENCES states(id)
 );
---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,10 @@
+-- A script that lists all the cities of california
+USE hbtn_0d_usa;
+
+SELECT id, name
+FROM cities
+WHERE state_id = (
+    SELECT id
+    FROM states
+    WHERE name = 'California'
+);
