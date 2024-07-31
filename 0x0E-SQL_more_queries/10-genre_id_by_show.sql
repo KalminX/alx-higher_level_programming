d86ed2d72e859085c99a7e4631164ac50adaeac7
KalminX <noriabure@gmail.com>
2024-07-31 07:58:44 +0100

    solved the eleventh task

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,9 @@
+-- A script that lists all shows contained in hbtn_0d_tvshows
+USE hbtn_0d_tvshows;
+SELECT
+    tv_shows.title AS title,
+    tv_shows_genres.genre_id AS genre_id
+FROM
+    tv_shows INNER JOIN tv_show_genres
+ORDER BY
+     title, genre_id;
