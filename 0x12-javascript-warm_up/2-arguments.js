7e9664f5336178ff728ac2172eb5a1713d0bbee3
KalminX <noriabure@gmail.com>
2024-07-31 11:51:39 +0100

    solved the third task

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+
+// print process.argv
+if (argv.length === 2) {
+  console.log('No argument');
+} else if (argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
