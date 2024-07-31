0b47307e9bfb51e93d44f26857b87395e071577e
KalminX <noriabure@gmail.com>
2024-07-31 05:09:53 +0100

    update: order script output by id

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,10 +1,9 @@
 -- A script that lists all the cities of california
 USE hbtn_0d_usa;
-
 SELECT id, name
 FROM cities
 WHERE state_id = (
     SELECT id
     FROM states
     WHERE name = 'California'
-);
+) ORDER BY id;
