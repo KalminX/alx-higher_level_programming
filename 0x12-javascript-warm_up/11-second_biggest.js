bb0670535f564926d4ece1a2fe4b9d46b78a1eb3
KalminX <noriabure@gmail.com>
2024-08-01 14:26:08 +0100

    adjusted the code to output second biggest instead of biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,10 +2,10 @@
 const { argv } = require('node:process');
 
 const numbers = argv.slice(2).map(x => Number(x));
-const biggestNumber = numbers.sort().reverse()[0];
+const secondBiggestNumber = numbers.sort().reverse()[1];
 
 if (argv.length === 2 || argv.length === 3) {
   console.log(0);
 } else {
-  console.log(biggestNumber);
+  console.log(secondBiggestNumber);
 }
