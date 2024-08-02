39bf7a773b1296a871c3566f9d00a0f223f071d1
KalminX <noriabure@gmail.com>
2024-08-02 23:25:07 +0100

    solved the last advanced task

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const {argv} = require('node:process');
+
+const fs = require('fs');
+
+const file1 = fs.readFileSync(`./${argv[2]}`);
+const file2 = fs.readFileSync(`./${argv[3]}`);
+const text = `${file1.toString()}\n${file2.toString()}`
+const file3 = fs.writeFileSync(argv[4], text);
