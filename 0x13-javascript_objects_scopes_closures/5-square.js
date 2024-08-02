28dcef1762cbd8db46f4aabe0f8a77ef5a4c170e
KalminX <noriabure@gmail.com>
2024-08-02 18:41:15 +0100

    fixed import issuesin sixth task

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,31 +1,6 @@
 #!/usr/bin/node
 
-class Rectangle {
-  constructor (w, h) {
-    if (w === undefined || h === undefined) {
-      return this;
-    }
-    if (!(w <= 0 || h <= 0)) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
-  }
-
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-  }
-
-  rotate () {
-    [this.width, this.height] = [this.height, this.width];
-  }
-}
+const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
   constructor (size) {
