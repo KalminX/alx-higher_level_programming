b8a52d4709580802af1e70b736771f133fcf00a7
KalminX <noriabure@gmail.com>
2024-08-02 21:24:49 +0100

    converted the variables to camel case

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const arr = require('./100-data').list;
 
-const new_arr = [...arr].map(x => x * arr.indexOf(x));
+const newArr = [...arr].map(x => x * arr.indexOf(x));
 
 console.log(arr);
-console.log(new_arr);
+console.log(newArr);
