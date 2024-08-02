c10e4ee80a5a15996b952ecd226185f9424e0791
KalminX <noriabure@gmail.com>
2024-08-02 16:42:49 +0100

    solved the sixth task

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,41 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor(w, h) {
+    if (w === undefined || h === undefined) {
+      return this;
+    }
+    if (!(w <= 0 || h <= 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      let span = "";
+      for (let i = 0; i < this.width; i++) {
+        span += "X";
+      }
+      console.log(span);
+    }
+  }
+  double() {
+    this.width += 2;
+    this.height *= 2;
+  }
+  rotate() {
+    let [x, y] = [this.width, this.height]
+    this.width = y;
+    this.height = x;
+  }
+}
+
+
+class Square extends Rectangle {
+    constructor (size) {
+        super(size, size);
+    }
+}
+
+module.exports = Rectangle;
+module.exports = Square;
