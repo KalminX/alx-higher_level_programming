9a73e8b2b97e7978c8c948452bd716be633a4cac
KalminX <noriabure@gmail.com>
2024-08-02 21:32:14 +0100

    removed extra array methods

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
 
-const newArr = [...list].map(x => x * list.indexOf(x));
+const newArr = [...list].map((num, index) => num * index);
 
 console.log(list);
 console.log(newArr);
