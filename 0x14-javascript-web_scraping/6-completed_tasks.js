68d500489da47cccc9cae0a6640a54f559920e2b
KalminX <noriabure@gmail.com>
2024-08-04 18:37:58 +0100

    fixed the semistandard errors

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,7 +2,6 @@
 
 const { argv } = require('node:process');
 const request = require('request');
-const fs = require('fs');
 
 request(argv[2], function (error, response, body) {
   if (error) {
@@ -10,22 +9,22 @@ request(argv[2], function (error, response, body) {
     return;
   }
   let tasks = JSON.parse(body);
-    try {
-      tasks = JSON.parse(body);
-    } catch (e) {
-      console.log("Invalid JSON:", e);
-      return;
-    }
+  try {
+    tasks = JSON.parse(body);
+  } catch (e) {
+    console.log('Invalid JSON:', e);
+    return;
+  }
 
   const completedTasks = tasks.filter(x => x.completed === true);
-  let tasksCount = {};
+  const tasksCount = {};
   completedTasks.forEach(x => {
-        const userId = x.userId;
-        if (!(userId in tasksCount)) {
-            tasksCount[userId] = 0;
-        } else {
-            tasksCount[userId]+=1; 
-        }
-    })
-    console.log(tasksCount);;
-  })
+    const userId = x.userId;
+    if (!(userId in tasksCount)) {
+      tasksCount[userId] = 0;
+    } else {
+      tasksCount[userId] += 1;
+    }
+  });
+  console.log(tasksCount);
+});
