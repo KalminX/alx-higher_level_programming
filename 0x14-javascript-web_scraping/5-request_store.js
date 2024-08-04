31d4d9bf8e21a16f1cc118b113572c6b89bdc6bc
KalminX <noriabure@gmail.com>
2024-08-04 18:00:10 +0100

    fixed the semistandard errors

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-const { argv } = require("node:process");
-const request = require("request");
-const fs = require("fs");
+const { argv } = require('node:process');
+const request = require('request');
+const fs = require('fs');
 
 request(argv[2], function (error, response, body) {
   if (error) {
-    console.log("error:", error);
+    console.log('error:', error);
   }
   fs.writeFile(argv[3], body, function (err) {
     if (err) {
