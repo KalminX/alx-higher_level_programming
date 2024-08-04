561101d70475f4247898742cd088a826d75e3cfa
KalminX <noriabure@gmail.com>
2024-08-04 17:59:00 +0100

    solved the sixth task

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,15 +1,11 @@
 #!/usr/bin/node
 
 const { argv } = require('node:process');
-
 const request = require('request');
-const url = `https://swapi-api.alx-tools.com/api/films/${argv[2]}`;
 
-request(url, function (error, response, body) {
+request(argv[2], function (error, response, body) {
   if (error) {
     console.log('error:', error);
   }
-  //   console.error("error:", error); // Print the error if one occurred
-  console.log(JSON.parse(body).title); // Print the response status code if a response was received
-//   console.log("body:", body); // Print the HTML for the Google homepage.
+  console.log(JSON.parse(body)); // Print the response status code if a response was received
 });
---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const { argv } = require("node:process");
+const request = require("request");
+const fs = require("fs");
+
+request(argv[2], function (error, response, body) {
+  if (error) {
+    console.log("error:", error);
+  }
+  fs.writeFile(argv[3], body, function (err) {
+    if (err) {
+      return console.log(err);
+    }
+  });
+});
